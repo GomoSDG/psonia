62b8d46d3419aaa0089b66b96603804d8f794170
Gomotso <gomotsol@navigare.co.za>
2020-09-16 07:13:20 +0200

    initial commit

---
+++ service.user/src/clj/psonia/users/core.clj
@@ -0,0 +1,27 @@
+(ns psonia.users.core
+  (:require [reitit.ring :as ring]
+            [org.httpkit.server :as server]))
+
+(defonce server (atom nil))
+
+(def app
+  (ring/ring-handler
+   (ring/router
+    [["/"  {:get (fn [req]
+                   {:status 200
+                    :body {:message "Healthy"}})}]
+     ["/users" {:get (fn [req]
+                       {:status 200
+                        :body {:message "Hello, world!"}})
+                :post (fn [req]
+                        {:status 200
+                         :body {:created true}})}]])))
+
+(defn start-server []
+  (when (nil? @server)
+    (reset! server (server/run-server #'app {:port 3000}))))
+
+
+(defn stop-server []
+  (when-not (nil? @server)
+    (@server)))
