b36a89de5bf96827126383c09c0392087ae57aac
Gomotso <gomotsol@navigare.co.za>
2020-09-20 08:55:54 +0200

    css and other stuff.

--- psonia.app/src/main/psonia/app/core.cljs
+++ psonia.app/src/main/psonia/app/core.cljs
@@ -4,13 +4,19 @@
               [psonia.app.layouts.site :refer [site-layout]]
               [psonia.app.routes :as routes :refer [hook-browser-navigation!]]
               [psonia.app.views :as views]
-              [re-frame.core :as re-frame]))
+              [re-frame.core :as re-frame]
+              [cljs.spec.gen.alpha :as gen]
+              [psonia.app.entities.products :as products]
+              [cljs.spec.alpha :as s]
+              [clojure.test.check]
+              [clojure.test.check.properties]))
 
 (re-frame/reg-event-db
  :initialize-db
  (fn [_ _]
    {:active-panel :home
-    :panels views/panels}))
+    :panels views/panels
+    :products (clojure.set/index (gen/sample (s/gen ::products/spec)) [:id])}))
 
 (defn render-active-panel []
   (let [active-panel @(re-frame/subscribe [:active-panel])]
--- psonia.app/src/main/psonia/app/panels/home.cljs
+++ psonia.app/src/main/psonia/app/panels/home.cljs
@@ -1,5 +1,19 @@
-(ns psonia.app.panels.home)
+(ns psonia.app.panels.home
+  (:require [psonia.app.panels.catalog.components :as comp-catalog]
+            [re-frame.core :as re-frame]
+            [reagent.ratom :as ratom]))
 
 (defn home []
-  [:h1 "Hello, Gomosdg!"])
+  (let [products (re-frame/subscribe [:products])]
+    (fn []
+      (js/console.log "Stuff!" (clj->js  @products))
+      [:<>
+       [:h1 "Hello, Gomosdg!"]
+       [:h2 "Products"]
+       [:section.px-lg-3.pt-4
+        (comp-catalog/product-grid @products)]])))
 
+(re-frame/reg-sub
+ :products
+ (fn [db]
+   (mapcat identity (vals (:products db)))))
