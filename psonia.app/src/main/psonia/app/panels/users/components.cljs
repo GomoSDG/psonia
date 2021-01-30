bfa13883180e7d5c1676fd60f632477f517fad0e
GomoSDG <lilokoego@outlook.com>
2021-01-30 16:04:59 +0200

    add users ns.

---
+++ psonia.app/src/main/psonia/app/panels/users/components.cljs
@@ -0,0 +1,10 @@
+(ns psonia.app.panels.users.components)
+
+(defn login-card
+  []
+  [:div.card.border-0.box-shadow
+   [:div.card-body
+    [:h2.h4.mb-1
+     "Sign in"]
+    [:div.py-3
+     ]])
---
+++ psonia.app/src/main/psonia/app/panels/users/core.cljs
@@ -0,0 +1,1 @@
+(ns psonia.app.panels.users.core)
